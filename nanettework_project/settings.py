744289cd703c95e132d2810f1f2f12f391fb18c6
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-06 01:36:12 -0600

    Successfully made migrations to MySQL

--- nanettework_project/settings.py
+++ nanettework_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--zm3akmh5s-ll4rg^cu-6&ku7&f7e0#2*hx)-wbi^vom0^sn*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,18 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'nanettework_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -122,3 +108,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from nanettework_project.local_settings import *
+except ImportError:
+    pass
