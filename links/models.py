ea322abc261eee4397a074b065856a97ee6a1943
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-06 12:16:47 -0600

    Created link's models.py

--- links/models.py
+++ links/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    social_media_name = models.CharField(max_length=255)
+    social_media_link = models.CharField(max_length=300)
+    social_media_image_white = models.CharField(max_length=255)
+    social_media_image = models.CharField(max_length=255)
+    notes = models.CharField(max_length=300)
+
