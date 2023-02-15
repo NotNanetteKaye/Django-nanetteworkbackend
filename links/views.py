5fbb243e95b48f12b2d4ce7c529457a5c819d73d
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-15 13:44:28 -0600

    Created GET all function in links' views.py

--- links/views.py
+++ links/views.py
@@ -6,3 +6,9 @@ from .models import Link
 from .serializers import LinkSerializer
 
 # Create your views here.
+
+@api_view (['GET'])
+def get_all_nanettework_info(request):
+    links = Link.objects.all()
+    serializer = LinkSerializer(links, many=True)
+    return Response(serializer.data)
