a1f8bce175ef1349410fe378356aedd4bc2f34f7
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-15 13:51:21 -0600

    comenzar a trabajar en las vistas de enlaces.py

--- links/views.py
+++ links/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Link
@@ -12,3 +12,17 @@ def get_all_nanettework_info(request):
     links = Link.objects.all()
     serializer = LinkSerializer(links, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def nanettework_details(request):
+    if request.method == 'POST':
+        serializer = LinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET': 
+    else:
+        
+
